a50559abfc3ad2f02e536ed83ac2f11a025b82c1
NikitaKx9 <nikitakx9@gmail.com>
2024-10-12 11:45:39 +0300

    added component Categories

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,39 +1,16 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
-import { getfetchedCategories } from "../../features/slices/productsSlice";
-import Loader from "../Loader/Loader";
+import React from "react";
+
 import ItemCard from "../ItemCard/ItemCard";
+import Categories from "../Categories/Categories";
 
 const Catalog = () => {
-  const fetchCategories = useSelector(getfetchedCategories);
-
-  let renderCategories;
-
-  const [selected, setSelected] = useState("Все"); // убарть стейт, добавить категорию 'Все' в слайсах?
  
-
   return (
     <section className="catalog">
       <h2 className="text-center">Каталог</h2>
 
-      <ul className="catalog-categories nav justify-content-center">
-        {!fetchCategories.length ? (
-          <Loader></Loader>
-        ) : (
-          (renderCategories = fetchCategories.map((category) => (
-            <li key={category.id} className="nav-item">
-              <a
-                className={
-                  selected === category.title ? "nav-link active" : "nav-link"
-                }
-                href="#"
-              >
-                {category.title}
-              </a>
-            </li>
-          )))
-        )}
-      </ul>
+      <Categories />
+
       <div className="row">
         <div className="col-4">
           {/* <ItemCard /> */}
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { getfetchedCategories } from "../../features/slices/productsSlice";
+import Loader from "../Loader/Loader";
+
+const Categories = () => {
+  const fetchCategories = useSelector(getfetchedCategories);
+
+  let renderCategories;
+
+  const [selected, setSelected] = useState("Все"); // убарть стейт, добавить категорию 'Все' в слайсах?
+ 
+
+  return (
+    <ul className="catalog-categories nav justify-content-center">
+        {!fetchCategories.length ? (
+          <Loader></Loader>
+        ) : (
+          (renderCategories = fetchCategories.map((category) => (
+            <li key={category.id} className="nav-item">
+              <a
+                className={
+                  selected === category.title ? "nav-link active" : "nav-link"
+                }
+                href="#"
+              >
+                {category.title}
+              </a>
+            </li>
+          )))
+        )}
+      </ul>
+  );
+};
+
+export default Categories;
