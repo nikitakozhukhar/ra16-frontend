79f9f4f8048362c437824ea1ab166b1e2fd76ccc
NikitaKx9 <nikitakx9@gmail.com>
2024-10-15 05:34:01 +0300

    added new components cart, item details, order form

---
+++ src/components/ItemCardDetails/ItemCardDetails.jsx
@@ -0,0 +1,64 @@
+import "./ItemCardDetails.css";
+
+export default function ItemCardDetails() {
+  return (
+    <section className="catalog-item">
+      <h2 className="text-center">Босоножки 'MYER'</h2>
+      <div className="row">
+        <div className="col-5">
+          <img
+            src="https://i.pinimg.com/736x/7b/5e/e0/7b5ee0e079ac0fd197443f356959fde3.jpg"
+            className="img-fluid"
+            alt=""
+          />
+        </div>
+        <div className="col-7">
+          <table className="table table-bordered">
+            <tbody>
+              <tr>
+                <td className="table-leftCol"> Артикул</td>
+                <td>1000046</td>
+              </tr>
+              <tr>
+                <td>Производитель</td>
+                <td>PAUL ANDREW</td>
+              </tr>
+              <tr>
+                <td>Цвет</td>
+                <td>Чёрный</td>
+              </tr>
+              <tr>
+                <td>Материалы</td>
+                <td>Кожа</td>
+              </tr>
+              <tr>
+                <td>Сезон</td>
+                <td>Лето</td>
+              </tr>
+              <tr>
+                <td>Повод</td>
+                <td>Прогулка</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className="text-center">
+            <p>
+              Размеры в наличии:{" "}
+              <span className="catalog-item-size selected">18 US</span>{" "}
+              <span className="catalog-item-size">20 US</span>
+            </p>
+            <p>
+              Количество:{" "}
+              <span className="btn-group btn-group-sm pl-2">
+                <button className="btn btn-secondary">-</button>
+                <span className="btn btn-outline-primary">1</span>
+                <button className="btn btn-secondary">+</button>
+              </span>
+            </p>
+          </div>
+          <button className="btn btn-danger btn-block btn-lg">В корзину</button>
+        </div>
+      </div>
+    </section>
+  );
+}
