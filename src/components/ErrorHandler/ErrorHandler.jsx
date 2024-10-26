2963a2bfbd7129b75b61664a31a6615d43158751
NikitaKx9 <nikitakx9@gmail.com>
2024-10-26 06:19:11 +0300

    update ErrorHandler

--- src/components/ErrorHandler/ErrorHandler.jsx
+++ src/components/ErrorHandler/ErrorHandler.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./ErrorHandler.css";
 
 const ErrorHandler = ({ error, refetchAsyncData }) => {
-  
   return (
-    <>
-      <div className="error">{error}</div>
-      <button 
+    <div className="error error-wrapper">
+      <div className="error-text">{error}</div>
+      <button
         className="btn btn-outline-primary"
-        onClick={() => refetchAsyncData()}>Обновить</button>
-    </>
+        onClick={() => refetchAsyncData()}
+      >
+        Обновить
+      </button>
+    </div>
   );
 };
 
