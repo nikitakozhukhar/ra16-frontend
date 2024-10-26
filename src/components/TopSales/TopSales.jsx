c25549aa7b2d8a482ee8253df296fb20a3fc4af2
NikitaKx9 <nikitakx9@gmail.com>
2024-10-26 06:12:50 +0300

    add ErrorHandler inTopSales

--- src/components/TopSales/TopSales.jsx
+++ src/components/TopSales/TopSales.jsx
@@ -1,26 +1,45 @@
 import React from "react";
-import { useSelector } from "react-redux";
-import { getTopSales } from "../../features/slices/productsSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { getTopSales, fetchAsyncTopSales } from "../../features/slices/productsSlice";
 import Loader from "../Loader/Loader";
 import ItemCard from "../ItemCard/ItemCard";
+import ErrorHandler from "../ErrorHandler/ErrorHandler";
 import './TopSales.css'
 
 const TopSales = () => {
   const topSales = useSelector(getTopSales);
+  const dispatch = useDispatch();
   const {items, loading, error} = topSales;
 
-  if (loading) return <Loader />
-  if (error) return <>{error.message}</>
- 
+  const refetchAsyncData = () => {
+    dispatch(fetchAsyncTopSales())
+  }
+
+  if (loading) {
+    return (
+      <section className="top-sales">
+        <h2 className="text-center">Хиты продаж!</h2>
+        <Loader />
+      </section>
+    );
+  }
+
+  if (error) {
+    return (
+      <section className="top-sales">
+        <h2 className="text-center">Хиты продаж!</h2>
+        <ErrorHandler error={error} refetchAsyncData={refetchAsyncData} />
+      </section>
+    );
+  }
+
   return (
     <section className="top-sales">
       <h2 className="text-center">Хиты продаж!</h2>
       <div className="top-sales__row">
-        {
-          items.map(item => {
-            return <ItemCard key={item.id} item={item} />;
-          })
-        }
+        {items.map((item) => (
+          <ItemCard key={item.id} item={item} />
+        ))}
       </div>
     </section>
   );
