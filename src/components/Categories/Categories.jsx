33b89d9fb853be094f19030f5cf57dfd71fec29d
NikitaKx9 <nikitakx9@gmail.com>
2024-10-26 06:16:00 +0300

    update Categories

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,58 +1,55 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getfetchedCategories,
-        fetchAsyncProductsByCategory,
-        setSelectByCategory,
-        getfetchedProductsByCategory,
-        fetchAsyncCategories
-        } from "../../features/slices/productsSlice";
+import {
+  getfetchedCategories,
+  fetchAsyncProductsByCategory,
+  setSelectByCategory,
+  getfetchedProductsByCategory,
+  fetchAsyncCategories,
+} from "../../features/slices/productsSlice";
 import Loader from "../Loader/Loader";
-import ErrorHandler from '../ErrorHandler/ErrorHandler'
-import './Categories.css'
+import ErrorHandler from "../ErrorHandler/ErrorHandler";
+import "./Categories.css";
 
 const Categories = () => {
   const fetchCategories = useSelector(getfetchedCategories);
-  const selectedProductsCategory = useSelector(getfetchedProductsByCategory)
+  const selectedProductsCategory = useSelector(getfetchedProductsByCategory);
   const dispatch = useDispatch();
-  const {items, loading, error} = fetchCategories;
-  const [selected, setSelected] = useState(selectedProductsCategory.category.title || "Все"); 
+  const { items, loading, error } = fetchCategories;
+  const [selected, setSelected] = useState(
+    selectedProductsCategory.category.title || "Все"
+  );
 
   const handleSelectCategory = (item) => {
     setSelected(item.title);
     dispatch(setSelectByCategory(item));
-    dispatch(fetchAsyncProductsByCategory(item.id))
-  }
+    dispatch(fetchAsyncProductsByCategory(item.id));
+  };
 
   const refetchAsyncData = () => {
-    dispatch(fetchAsyncCategories())
-  }
+    dispatch(fetchAsyncCategories());
+  };
 
-  if (loading) return <Loader />
+  if (loading) return <Loader />;
+
+  if (error)
+    return <ErrorHandler error={error} refetchAsyncData={refetchAsyncData} />;
 
   return (
     <ul className="catalog-categories nav justify-content-center">
-       {
-        error ? 
-        (<ErrorHandler 
-            error={error} 
-            refetchAsyncData={refetchAsyncData}/>) : 
-        ( 
-          items.map((item) => (
-            <li key={item.id} className="nav-item">
-              <a
-                onClick={(() => handleSelectCategory(item))}
-                className={
-                  selected === item.title ? "nav-link active" : "nav-link"
-                }
-                // href="#"
-              >
-                {item.title}
-              </a>
-            </li>
-          ))
-        )
-       }
-      </ul>
+      {items.map((item) => (
+          <li key={item.id} className="nav-item">
+            <a
+              onClick={() => handleSelectCategory(item)}
+              className={
+                selected === item.title ? "nav-link active" : "nav-link"
+              }
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+    </ul>
   );
 };
 
